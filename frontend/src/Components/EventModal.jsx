4df1848ba8b41c6fce7809a54e46a600c8fb32e8
Sandali Disara <sandalidissanayake123@gmail.com>
2024-04-08 12:41:15 +0530

    create Add Event Modal

--- frontend/src/Components/EventModal.jsx
+++ frontend/src/Components/EventModal.jsx
@@ -8,14 +8,18 @@ export default function EventModal ({ event, onClose }) {
     <div className="modal">
       <div className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
-        <h2>{event.title}</h2>
-        <p>{event.description}</p>
-        <p>Host: {event.host}</p>
-        <p>Start Time: {new Date(event.startTime).toLocaleString()}</p>
-        <p>End Time: {new Date(event.endTime).toLocaleString()}</p>
+        <h2 className="title">{event.title}</h2>
+        <div className="description">{event.description}</div>
+        <div className="horizontal-line"></div>
+        <div className="time">
+          <p className="date-time">Start Time: {new Date(event.startTime).toLocaleString()}</p>
+          <p className="date-time">End Time: {new Date(event.endTime).toLocaleString()}</p>
+        </div>
+        <div className="host">
+          <p className="date-time">Host: {event.host}</p>
+          <p className="date-time">Date: {event.date}</p>
+        </div>
       </div>
     </div>
   );
 };
-
-
