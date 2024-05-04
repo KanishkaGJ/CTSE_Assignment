deab0851437eaf0e745c8eb47ddd163a1acf72ed
Kulanaka Bandara <mkulanakabandara@gmail.com>
2024-05-04 14:10:42 +0530

    app.jsx change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AllEvents from './Components/AllEvents';
 
-export default function App() {
+export default function App() { // default app
   return (
     <Router>
       
